94157ecd173ecbd62cd40b27579ad4f7b254da3f
Daniel Hernandez Carrasco <dhe72@uclive.ac.nz>
2025-09-15 15:52:53 +1200

    clean repo [skip ci]

--- R/gdmmTMB-package.R
+++
@@ -1,9 +0,0 @@
-#' @keywords internal
-"_PACKAGE"
-
-## usethis namespace: start
-#' @importFrom lme4 nobars
-#' @importFrom vegan designdist
-#' @importFrom vegan vegdist
-## usethis namespace: end
-NULL
